fb75f0db470748a341b5245e894d64160d2ec32b
lucastso10 <lucastso10@gmail.com>
2023-03-27 13:21:18 +0000

    simple changes

--- main.py
+++ main.py
@@ -2,37 +2,11 @@ import random
 
 card_types = ["R", "G", "B", "Y", "S"]
 
-card_number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "R", "B"]
+card_attribute = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "R", "B", "+2"]
 
 def main():
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  print("bom dia!")
+  return
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,17 +1,29 @@
 
 class Card:
-    def __init__(self, cardType, attribute):
-        self.cardType = cardType
-        self.attribute = attribute
-        
-    def __str__(self):
-        return f"{self.cardType} {self.attribute}"
+  def __init__(self, cardType, attribute):
+    self.cardType = cardType
+    self.attribute = attribute
+      
+  def __str__(self):
+    return f"{self.cardType} {self.attribute}"
+
+  def playable(self, card):
+    if self.cardType == "S":
+      return True
+    elif self.cardType == card.cardType:
+      return True
+    elif self.attribute == card.attribute:
+      return True
+    else:
+      return False
 
 
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.cards = []
-        
-    def __str__(self):
-        return f"{self.name}"
+  def __init__(self, name):
+    self.name = name
+    self.cards = []
+      
+  def __str__(self):
+    return f"{self.name}"
+
+    
